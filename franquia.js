5926b6271e9e1fe70287964974093ebdefb9892d
Bruno Leonardo Sancho Ventura <86660522+brunoventura123@users.noreply.github.com>
2021-10-30 13:21:31 -0300

    Subindo todos os arquivos

---
+++ franquia.js
@@ -0,0 +1,9 @@
+
+function menuTogle(){
+    document.querySelector('nav');
+    if(document.querySelector('nav').style.display == 'none') {
+        document.querySelector('nav').style.display = 'block';
+    } else{
+        document.querySelector('nav').style.display = 'none';
+    }
+}
