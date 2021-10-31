3df7ee1601852c4d46e9d26739d5f0bbadade13d
Bruno Leonardo Sancho Ventura <86660522+brunoventura123@users.noreply.github.com>
2021-10-31 12:18:57 -0300

    Correção do css

--- franquia.js
+++
@@ -1,9 +0,0 @@
-
-function menuTogle(){
-    document.querySelector('nav');
-    if(document.querySelector('nav').style.display == 'none') {
-        document.querySelector('nav').style.display = 'block';
-    } else{
-        document.querySelector('nav').style.display = 'none';
-    }
-}
